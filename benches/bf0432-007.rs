1c2397b38dd59ff4cfd0177ee46a0237a2782208
mxheller <heller.max1@gmail.com>
2020-04-09 13:55:00 -0400

    benchmarks

---
+++ benches/bf0432-007.rs
@@ -0,0 +1,23 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use sat::*;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+pub fn bf0432(c: &mut Criterion) {
+    let lines = BufReader::new(File::open("inputs/bf0432-007.cnf").unwrap())
+        .lines()
+        .map(Result::unwrap)
+        .collect::<Vec<_>>();
+    c.bench_function("bf0432-007 solve", |b| {
+        b.iter(|| Formula::parse_and_solve(&lines).unwrap())
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = bf0432
+}
+criterion_main!(benches);
